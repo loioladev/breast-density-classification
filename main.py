e9326b71e3751ce11fb1fe2e1f0185cb0180b80e
Matheus Loiola <matheusloiolapinto@gmail.com>
2024-10-25 20:41:48 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def create_parser() -> argparse.ArgumentParser:
+    """ Create parser """
+    parser = argparse.ArgumentParser(description='Breast density training')
+    return parser
+
+
+def main() -> None:
+    """ Main function """
+
+
+if __name__ == '__main__':
+    main()
