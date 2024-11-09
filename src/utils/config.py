1464a80a14bc09ffa735f44320c8e79b949b517e
loioladev <matheusloiolapinto@gmail.com>
2024-11-09 19:38:48 -0300

    feat: added optimizers, schedulers and losses

--- src/utils/config.py
+++ src/utils/config.py
@@ -7,6 +7,89 @@ import random
 
 import numpy
 import torch
+import torch.nn as nn
+from torch.optim import Optimizer, lr_scheduler
+import logging
+import sys
+from focal_loss.focal_loss import FocalLoss # https://github.com/mathiaszinnen/focal_loss_torch
+
+logger = logging.getLogger()
+
+
+class ConfigManager:
+    @staticmethod
+    def get_optimizer(model: nn.Module, optimizer: str, args: dict) -> Optimizer:
+        """
+        Get optimizer instance
+
+        :param model: The model instance
+        :param optimizer: The optimizer name
+        :param args: The arguments for the optimizer
+        :return optimizer: The optimizer instance
+        """
+        optimizers = {
+            "adamw": torch.optim.AdamW,
+            "adam": torch.optim.Adam,
+            "sgd": torch.optim.SGD,
+            "rsmprop": torch.optim.RMSprop,
+        }
+        if optimizer not in optimizers:
+            logger.error(f"Optimizer {optimizer} not implemented")
+            sys.exit(1)
+        optimizer = optimizers[optimizer](model.parameters(), **args)
+        return optimizer
+
+    @staticmethod
+    def get_scheduler(optimizer: Optimizer, scheduler: str, args: dict) -> torch.optim.lr_scheduler._LRScheduler:
+        """
+        Get scheduler instance
+
+        :param optimizer: The optimizer instance
+        :param scheduler: The scheduler name
+        :param args: The arguments for the scheduler
+        :return scheduler: The scheduler instance
+        """
+        schedulers = {
+            "step": lr_scheduler.StepLR,
+            "multistep": lr_scheduler.MultiStepLR,
+            "plateau": lr_scheduler.ReduceLROnPlateau,
+            "cosine": lr_scheduler.CosineAnnealingLR,
+            "wamup_cosine": lr_scheduler.CosineAnnealingWarmRestarts,
+        }
+        if scheduler not in schedulers:
+            logger.error(f"Scheduler {scheduler} not implemented")
+            sys.exit(1)
+        
+        scheduler = schedulers[scheduler](optimizer, **args)
+        return scheduler
+
+    @staticmethod
+    def get_loss(loss: str, weights: list, args: dict) -> nn.Module:
+        """
+        Get loss function instance
+
+        :param loss: The loss function name
+        :param weights: The weights for the loss function
+        :param args: The arguments for the loss function
+        :return loss: The loss function instance
+        """
+        losses = {
+            "cross_entropy": nn.CrossEntropyLoss,
+            "mse": nn.MSELoss,
+            "focal": FocalLoss
+        }
+        if loss not in losses:
+            logger.error(f"Loss {loss} not implemented")
+            sys.exit(1)
+        
+        if loss == 'cross_entropy' and not args['weight']:
+            del args['weight']
+        # TODO: review this line, improving it
+        if loss == 'focal':
+            args['weights'] = torch.tensor(weights).to(set_device())
+
+        loss = losses[loss](**args)
+        return loss
 
 
 def set_seed(seed: int) -> None:
--- src/utils/loss.py
+++
@@ -1,4 +0,0 @@
-"""
-This module contains the losses functions to be used in the training
-of the model.
-"""
