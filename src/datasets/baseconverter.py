053dc338c6997aae38c428b0b918855d160289e5
loioladev <matheusloiolapinto@gmail.com>
2024-12-12 01:30:33 -0300

    feat: added abstract class to datasets

---
+++ src/datasets/baseconverter.py
@@ -0,0 +1,67 @@
+import multiprocessing
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+import pandas as pd
+
+
+class BaseConverter(ABC):
+    """
+    Base class for dataset converters
+    """
+
+    def __init__(self, dataset_dir: str, dataset_output: str) -> None:
+        """
+        Constructor for the BaseConverter class
+
+        :param dataset_dir: Path to the dataset directory
+        :param dataset_output: Path to the output directory
+        """
+        self.dataset_dir = Path(dataset_dir)
+        self.dataset_output = Path(dataset_output)
+
+    def start_dicom_conversion(self, files: list[Path], workers: int = 1) -> None:
+        """
+        Start the DICOM conversion process with the given files. It creates a new directory
+        called 'images' in the output directory and saves the converted images there
+
+        :param files: List of DICOM files
+        :param output: Path to the output directory
+        :param workers: Number of workers to use with multiprocessing
+        """
+        output = self.dataset_output / "images"
+        output.mkdir(parents=True, exist_ok=True)
+        with multiprocessing.Pool(workers) as pool:
+            pool.starmap(
+                self.process_dicom,
+                [(file, output) for file in files],
+            )
+
+    @abstractmethod
+    def process_dicom(self):
+        """
+        Convert the DICOM file to PNG with the respect transformations of the dataset
+        and save it in the output directory
+        """
+        pass
+
+    @abstractmethod
+    def process_csv(self) -> None:
+        """Convert the original CSV file to the desired format and save it to the output directory"""
+        pass
+
+    @abstractmethod
+    def convert_dataset(self) -> None:
+        """Convert the original dataset to the desired format and save it to the output directory."""
+        pass
+
+    @abstractmethod
+    def get_dataset(self, csv_path: str | Path, image_path: str | Path) -> pd.DataFrame:
+        """
+        From the converted CSV file, create a DataFrame with the columns 'path' and 'target'.
+
+        :param csv_path: Path to the CSV file
+        :param image_path: Path to the images directory
+        :return: DataFrame with the path and target columns
+        """
+        pass
--- src/datasets/bmcd.py
+++ src/datasets/bmcd.py
@@ -8,163 +8,147 @@ extracted bcmd dataset folder (usually 'Dataset').
 
 import logging
 import os
+from pathlib import Path
 
 import cv2
 import numpy as np
 import pandas as pd
 import pydicom
-from pathlib import Path
-import multiprocessing
 
+from src.datasets.baseconverter import BaseConverter
 from src.utils.processing import recort_breast_morp
 
 logger = logging.getLogger()
 
 
-def process_bmcd_images(dicom_path: Path, output: Path) -> None:
+class BMCDConverter(BaseConverter):
     """
-    Process the DICOM file to the output directory
-
-    :param dicom_path: Path to the DICOM file
-    :param output: Path to the output directory
+    BMCD dataset converter
     """
-    # -- read dicom file
-    ds = pydicom.dcmread(dicom_path)
 
-    # -- read image and convert to uint8
-    image = pydicom.pixel_array(ds)
-    image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    def process_dicom(self, dicom_path: Path, output: Path) -> None:
+        """
+        Process the DICOM file to the output directory
 
-    # -- crop breast region
-    image, _ = recort_breast_morp(image)
+        :param dicom_path: Path to the DICOM file
+        :param output: Path to the output directory
+        """
+        # -- read dicom file
+        ds = pydicom.dcmread(dicom_path)
 
-    # -- flip image if it is right breast
-    if ds.ImageLaterality == "R":
-        image = cv2.flip(image, 1)
+        # -- read image and convert to uint8
+        image = pydicom.pixel_array(ds)
+        image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
 
-    # -- create new name for image and save it
-    file_id = dicom_path.parent.name
-    view = dicom_path.stem.split("_")[0]
-    file_info = dicom_path.stem.split("_")[1]
-    file_class = dicom_path.parent.parent.name[0]
-    file_name = f"{file_class}_{file_id}_{view}_{file_info}.png".lower()
-    cv2.imwrite(str(Path(output) / file_name), image)
+        # -- crop breast region
+        image, _ = recort_breast_morp(image)
 
+        # -- flip image if it is right breast
+        if ds.ImageLaterality == "R":
+            image = cv2.flip(image, 1)
 
-def process_dicom(
-    dicom_dir: str, output: str, processes: int = 1
-) -> None:
-    """
-    Process the DICOM files in the INbreast dataset
+        # -- create new name for image and save it
+        file_id = dicom_path.parent.name
+        view = dicom_path.stem.split("_")[0]
+        file_info = dicom_path.stem.split("_")[1]
+        file_class = dicom_path.parent.parent.name[0]
+        file_name = f"{file_class}_{file_id}_{view}_{file_info}.png".lower()
+        cv2.imwrite(str(Path(output) / file_name), image)
 
-    :param dicom_dir: Path to the DICOM directory
-    :param output: Path to the output directory
-    :param dicom_ids: Set of DICOM IDs to be processed
-    :param processes: Number of processes to use
-    """
-    logger.info("Processing DICOM files")
-    output = os.path.join(output, "images")
-    os.makedirs(output, exist_ok=True)
+    def process_csv(self, xlsx_path: Path, dicom_dir: Path) -> None:
+        """
+        Create a CSV file with the information of the DICOM files
 
-    # -- read dicom files and convert to 8-bit images
-    files = list(dicom_dir.rglob("*.dcm"))
-    with multiprocessing.Pool(processes) as pool:
-        pool.starmap(
-            process_bmcd_images,
-            [(file, output) for file in files],
+        :param xlsx_path: Path to the XLSX file
+        :param dicom_dir: Path to the DICOM directory
+        """
+        normal_cases = pd.read_excel(xlsx_path, sheet_name="Normal_cases", skiprows=2)
+        suspicious_cases = pd.read_excel(
+            xlsx_path, sheet_name="Suspicious_cases", skiprows=2
         )
-    logger.info(f"DICOM files done with {processes} processes")
 
+        files = list(dicom_dir.rglob("*.dcm"))
+        data = []
+        for file in files:
+            ds = pydicom.dcmread(file, specific_tags=["ImageLaterality"])
+            file_idx = file.parent.name
+            view = file.stem.split("_")[0]
+            file_info = file.stem.split("_")[1]
+            file_class = file.parent.parent.name[0]
 
-def process_csv(xlsx_path: Path, dicom_dir: Path, output: Path) -> None:
-    """
-    Create a CSV file with the information of the DICOM files
+            if file_class == "s":
+                xls = suspicious_cases
+            else:
+                xls = normal_cases
+            density = xls.loc[
+                xls["Folder #"] == int(file_idx),
+                "BI-RADS categories for breast density",
+            ].values[0]
 
-    :param xlsx_path: Path to the XLSX file
-    :param dicom_dir: Path to the DICOM directory
-    :param output: Path to the output directory
-    """
+            data.append(
+                {
+                    "case": file_class.lower(),
+                    "number": int(file_idx),
+                    "status": file_info.lower(),
+                    "laterality": ds.ImageLaterality.lower(),
+                    "view": view.lower(),
+                    "density": density.lower(),
+                }
+            )
+        dataframe = pd.DataFrame(data)
+        dataframe.to_csv(self.dataset_output / "metadata.csv", index=False)
+        logger.info("CSV file created")
 
-    normal_cases = pd.read_excel(xlsx_path, sheet_name="Normal_cases", skiprows=2)
-    suspicious_cases = pd.read_excel(xlsx_path, sheet_name="Suspicious_cases", skiprows=2)
+    def convert_dataset(self, processes: int = 1) -> None:
+        """
+        Convert the BMCD dataset to PNG images and save them to the output directory
+        with a CSV file containing the information of the images.
 
-    files = list(dicom_dir.rglob("*.dcm"))
-    data = []
-    for file in files:
-        ds = pydicom.dcmread(file, specific_tags=["ImageLaterality"])
-        file_idx = file.parent.name
-        view = file.stem.split("_")[0]
-        file_info = file.stem.split("_")[1]
-        file_class = file.parent.parent.name[0]
+        :param path: Path to the BMCD dataset
+        :param output: Path to the output directory
+        :param processes: Number of processes to use
+        """
+        logger.info("Processing BMCD dataset")
 
-        if file_class == 's':
-            xls = suspicious_cases
-        else:
-            xls = normal_cases
-        density = xls.loc[xls["Folder #"] == int(file_idx), "BI-RADS categories for breast density"].values[0]
-        
-        data.append(
-            {
-                "case": file_class.lower(),
-                "number": int(file_idx),
-                "status": file_info.lower(),
-                "laterality": ds.ImageLaterality.lower(),
-                "view": view.lower(),
-                "density": density.lower()
-            }
+        if not self.dataset_dir.exists():
+            raise FileNotFoundError(f"Path {self.dataset_dir} does not exist")
+
+        dicomdir = self.dataset_dir / "Dataset"
+        xlsx_path = self.dataset_dir / "bmcd.xlsx"
+
+        if not os.path.exists(dicomdir):
+            raise FileNotFoundError(f"Path {dicomdir} does not exist")
+
+        if not os.path.exists(xlsx_path):
+            raise FileNotFoundError(f"Path {xlsx_path} does not exist")
+
+        dicom_files = list(dicomdir.rglob("*.dcm"))
+        self.start_dicom_conversion(dicom_files, processes)
+        self.process_csv(xlsx_path, dicomdir)
+
+        logger.info("BMCD dataset processed")
+
+    def get_dataset(self, csv_path: str | Path, image_path: str | Path) -> pd.DataFrame:
+        """
+        Modify the BMCD dataset to the training model format, adding the columns
+        'path' and 'target' to the DataFrame.
+
+        :param csv_path: Path to the CSV file
+        :param image_path: Path to the images directory
+        :return: DataFrame with the path and target columns
+        """
+        if isinstance(csv_path, str):
+            csv_path = Path(csv_path)
+        if isinstance(image_path, str):
+            image_path = Path(image_path)
+
+        df = pd.read_csv(csv_path)
+        df["target"] = df["density"].apply(
+            lambda x: {"a": 0, "b": 1, "c": 2, "d": 3}[x]
         )
-    dataframe = pd.DataFrame(data)
-    dataframe.to_csv(output / "metadata.csv", index=False)
-    logger.info("CSV file created")
-
-
-def convert_bmcd(path: str, output: str, processes: int = 1) -> None:
-    """
-    Convert the BMCD dataset to PNG images and save them to the output directory
-    with a CSV file containing the information of the images.
-
-    :param path: Path to the INbreast dataset
-    :param output: Path to the output directory
-    :param processes: Number of processes to use
-    """
-    logger.info("Processing BMCD dataset")
-
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"Path {path} does not exist")
-    
-    path = Path(path)
-    dicomdir = path / "Dataset"
-    xlsx_path = path / "bmcd.xlsx"
-    output_path = Path(output)
-
-    if not os.path.exists(dicomdir):
-        raise FileNotFoundError(f"Path {dicomdir} does not exist")
-    
-    if not os.path.exists(xlsx_path):
-        raise FileNotFoundError(f"Path {xlsx_path} does not exist")
-    
-    process_dicom(dicomdir, output_path, processes)
-    process_csv(xlsx_path, dicomdir, output_path)
-
-    logger.info("BMCD dataset processed")
-
-    
-def get_bmcd(csv_path: str, image_path: str) -> pd.DataFrame:
-    """
-    Modify the BMCD dataset to the training model format, adding the columns
-    'path' and 'target' to the DataFrame.
-
-    :param csv_path: Path to the CSV file
-    :param image_path: Path to the images directory
-    :return: DataFrame with the path and target columns
-    """
-    df = pd.read_csv(csv_path)
-    df["target"] = df["density"].apply(lambda x: {"a": 0, "b": 1, "c": 2, "d": 3}[x])
-    df["path"] = df.apply(
-        lambda row: os.path.join(
-            image_path, f"{row['case']}_{row['number']}_{row['view']}_{row['status']}.png"
-        ),
-        axis=1,
-    )
-    df = df[["path", "target"]]
-    return df
+        df["path"] = df.apply(
+            lambda row: image_path
+            / f"{row['case']}_{row['number']}_{row['view']}_{row['status']}.png"
+        )
+        df = df[["path", "target"]]
+        return df
